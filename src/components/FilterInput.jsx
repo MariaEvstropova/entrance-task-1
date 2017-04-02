cb229f8401c7dc9caa15e74be5b83ea6425da0d3
Maria Evstropova <m.evstropova@yandex.ru>
2017-04-02 13:11:10 +0300

    filtration

--- src/components/FilterInput.jsx
+++ src/components/FilterInput.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 export default class FilterInput extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            value: "",
+        };
+        this.handleTextChange = this.handleTextChange.bind(this);
+    }
+
+    handleTextChange(event) {
+        let value = event.target.value;
+        this.setState({
+            value: value
+        });
+        this.props.handleFilterChange(this.props.inputName, value);
+    }
+
     render() {
         return (
         <div className="filter-option">
             <label htmlFor={this.props.inputName + "-filter"}>{this.props.labelText}</label>
-            <input id={this.props.inputName + "-filter"} type="text" />
+            <input id={this.props.inputName + "-filter"} type="text" value={this.state.value} onChange={this.handleTextChange} />
         </div>
         );
     }
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,15 +4,71 @@ import Table from './Table.jsx';
 import {data} from './tableData';
 
 export default class MainContent extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            school: "",
+            teacher: "",
+            date: ""
+        };
+        this.handleFilterChange = this.handleFilterChange.bind(this);
+        this.filterData = this.filterData.bind(this);
+    }
+
+    handleFilterChange(type, value) {
+        this.setState({
+            [type]: value
+        })
+    }
+
+    filterData(schoolFilter, teachrFilter, dateFilter, data) {
+        let regexSchool = new RegExp(schoolFilter, 'i');
+        let regexTeacher = new RegExp(teachrFilter, 'i');
+        let regexDate = new RegExp(dateFilter, 'i');
+
+        let result = data.filter((item) => {
+            //Т.к. школы могут быть как массивом, так и строкой рассматриваем 2 варианта
+            let schoolTest = false;
+            if (Array.isArray(item.school)) {
+                for (let i = 0; i < item.school.length; i++) {
+                    if (regexSchool.test(item.school[i])) {
+                        schoolTest = true;
+                        break;
+                    }
+                }
+            } else {
+                schoolTest = regexSchool.test(item.school);
+            }
+            //Т.к. лекторы могут быть как массивом, так и строкой рассматриваем 2 варианта
+            let teacherTest = false;
+            if (Array.isArray(item.teacher)) {
+                for (let i = 0; i < item.teacher.length; i++) {
+                    if (regexTeacher.test(item.teacher[i])) {
+                        teacherTest = true;
+                        break;
+                    }
+                }
+            } else {
+                teacherTest = regexTeacher.test(item.teacher);
+            }
+
+            return (
+                schoolTest && teacherTest && regexDate.test(item.date)
+            );
+        });
+
+        return result;
+    }
+
     render() {
         return (
             <div>
                 <div className="filter-options">
-                    <FilterInput inputName="school" labelText="Школа"/>
-                    <FilterInput inputName="teacher" labelText="Лектор"/>
-                    <FilterInput inputName="date" labelText="Дата"/>
+                    <FilterInput inputName="school" labelText="Школа" handleFilterChange={this.handleFilterChange}/>
+                    <FilterInput inputName="teacher" labelText="Лектор" handleFilterChange={this.handleFilterChange}/>
+                    <FilterInput inputName="date" labelText="Дата" handleFilterChange={this.handleFilterChange}/>
                 </div>
-                <Table tableData={data}/>
+                <Table tableData={this.filterData(this.state.school, this.state.teacher, this.state.date, data)}/>
             </div>
         );
     }
